3d8ebac5b79b43788e9b8c75376386269254ed2f
Dhruv Naik <dhruvrnaik@gmail.com>
2022-11-09 23:41:26 +0000

    add scripts, data and readme for textual inversion

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sd-comic-generation
+# Setup VM
+- TPU type: `v3-8`
+- TPU software version: `tpu-base-vm` 
+## Install JAX
+`pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html`
+
+## Install JAX-SMI
+`sudo apt install golang`
+`pip install jax-smi`
+
---
+++ scripts/textual inversion/run_flax_inversion.sh
@@ -0,0 +1,13 @@
+export MODEL_NAME="duongna/stable-diffusion-v1-4-flax"
+export DATA_DIR="/home/dhruvnaik/sample_dataset"
+
+python3 textual_inversion_flax.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --train_data_dir=$DATA_DIR \
+  --learnable_property="object" \
+  --placeholder_token="<cat-toy>" --initializer_token="toy" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --max_train_steps=3 \
+  --learning_rate=5.0e-04 --scale_lr \
+  --output_dir="textual_inversion_cat_test"
